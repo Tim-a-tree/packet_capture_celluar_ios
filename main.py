0496f143f186695064a1ce30e7965fd77fa33889
Tim-a-tree <jeonghyunheo619@gmail.com>
2023-10-11 16:15:07 +0900

    Implement: rvi_capture

--- main.py
+++ main.py
@@ -1,18 +1,50 @@
 from sys import platform
-import pymobiledevice3.usbmux
+from pymobiledevice3 import usbmux, lockdown, services
+import rvi_capture_copy as rvi
+
+# WARNING: on development,
+# def get_interface(udid):
+#     # get the lockdown client for the first connected device
+#     client = lockdown.create_using_usbmux()
+
+#     # get the network information
+#     network_info = client.get_value("com.apple.mobile.data_sync", "com.apple.mobile.wireless_lockdown", "WiFiNetworkInfo", udid)
+
+
+#     # extract the interface name
+#     interface = network_info["InterfaceName"]
+    
+
+#     return interface
+
+# auto-detection
+# def auto_detect():
+
+
+# NOTE: currently only detects when the device is connected
+# TODO: if the device is not connected, wait for the device to be connected
+# TODO: realtime packet capture
+# TODO: changing the packet capture algorithm with C code (using libimobiledevice)
+# TODO: able to select the device from the multiple devices
 
 def main():
     # gets the list of connected device
     # NOTE: only shows number, udid, type 
-    list_device = pymobiledevice3.usbmux.list_devices()
+    device_name = usbmux.list_devices()
+    if len(device_name) == 0:
+        print("Please connect the device and start the program again")
+        input("Press enter to exit;")
+        quit()
 
-    print(list_device)
+    print(device_name)
+    udid = device_name[0].serial
+    print("UDID: ", udid) # WARNING: only gets the first device from the list, it will be updated later
+    # print(get_interface(udid))
 
-    # gets the UDID of the device
-    # WARNING: only gets the first device from the list, it will be updated later
-    print("UDID: ", list_device[0].serial)
-
-    # TODO: connect to the device and start capturing the packets
+    file_name = input("Please enter the name of the file: ")
+    file_name = file_name + ".pcapng"
+    print("\n\nStart Capturing the packets from the device, in order to stop the capture press 'Ctrl + C'\n\n")
+    rvi.start_capture(udid, file_name)
 
 
 
