d1315ab6bd4fd692f8d9c9390b0fed612fbb7e5d
Tim-a-tree <jeonghyunheo619@gmail.com>
2023-11-15 13:48:19 +0900

    MODIFY: Device json for unifying with frontend

--- device.py
+++ device.py
@@ -1,9 +1,9 @@
 class Device:
-    def __init__(self, name, device_type, device_serial, udid):
+    def __init__(self, name, device_type, device_id, device_udid):
         self.device_name = name
         self.device_type = device_type
-        self.device_serial = device_serial
-        self.udid = udid
+        self.device_id = device_id
+        self.device_udid = device_udid
         # self.build_version = build_version
         # self.product_version = product_version
 
@@ -11,8 +11,8 @@ def to_json(Device):
     return {
         "device_name": Device.device_name,
         "device_type": Device.device_type,
-        "device_serial": Device.device_serial,
-        "udid": Device.udid,
+        "device_id": Device.device_id,
+        "device_udid": Device.device_udid,
         # "build_version": Device.build_version,
         # "product_version": Device.product_version
 
