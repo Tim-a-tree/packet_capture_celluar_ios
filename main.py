92ff7d5abac65fef51a85d43095e7015cbd6f8eb
JeongHyunHeo <59818387+Tim-a-tree@users.noreply.github.com>
2024-01-05 07:52:25 +0900

    FIX: live capture option (#2)

--- main.py
+++ main.py
@@ -7,11 +7,13 @@ import calendar
 
 # TODO: changing the packet capture algorithm with C code (using libimobiledevice)
 
+
 # auto-detection
 def auto_detect():
     devices = usbmux.list_devices()
-    
+
     return devices
+
 
 def main():
     # if no device is connected, wait until the device is connected
@@ -24,7 +26,7 @@ def main():
             break
 
     # gets the list of connected device
-    # NOTE: only shows number, udid, type 
+    # NOTE: only shows number, udid, type
     print("----------------- Connected Devices -----------------")
     for i in range(len(device_name)):
         print(i, device_name[i].serial)
@@ -41,21 +43,20 @@ def main():
     device_serial = client.get_value(None, "SerialNumber")
     device_udid = client.get_value(None, "UniqueDeviceID")
 
-
     file_name = device_info + "_" + str(calendar.timegm(time.gmtime())) + ".pcapng"
 
     # live capture option
     print("Live Capture?(Y/N)")
     live = input()
 
-    print("Start Capturing the packets from the device, in order to stop the capture press 'Ctrl + C'\n\n")
+    print(
+        "Start Capturing the packets from the device, in order to stop the capture press 'Ctrl + C'\n\n"
+    )
 
-    if live:
+    if live.lower() == "y":
         rvi.start_live_capture(udid, file_name)
     else:
         rvi.start_capture(udid, file_name)
-    
-
 
 
 if __name__ == "__main__":
